eeb5c4c403fa93c7c0137d1836713b2a63d8b9ff
Diego C Rodrigues <diego@debian.com>
2021-04-11 21:43:43 -0300

    feat(Cases): cases page Mobile/Web

--- src/Pages/Web/Cases/index.tsx
+++ src/Pages/Web/Cases/index.tsx
@@ -1,10 +1,120 @@
-import { Typography } from '@material-ui/core';
+import {
+  Typography,
+  Grid,
+  TextField,
+  InputAdornment,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+} from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import React from 'react';
-import { Container } from './styles';
+import { Container, useStyles } from './styles';
 const Cases: React.FC = () => {
+  const classes = useStyles();
   return (
     <Container>
-      <Typography>Hello from Cases</Typography>
+      <Grid container direction="column" justify="flex-start" alignItems="baseline">
+        <Grid item>
+          <Typography align="justify">
+            <h1 style={{ color: '#1351B4', fontSize: 38, marginBottom: '-40px' }}>Conheça os</h1>
+            <h1 style={{ color: '#1351B4', fontSize: 38, marginBottom: '-20px' }}>cases,</h1>
+            <h2 style={{ color: '#1351B4', fontSize: 18, marginBottom: '-25px' }}>
+              Algumas decisões <br /> judiciais podem te ajudar!
+            </h2>
+          </Typography>
+        </Grid>
+        <Grid item style={{ marginTop: '40px' }}>
+          <TextField
+            style={{ width: '300px' }}
+            label="Experimente buscar por alguma coisa"
+            fullWidth
+            variant="filled"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+      </Grid>
+      <Grid container direction="row" justify="flex-start" alignItems="baseline">
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography className={classes.title} variant="h6" component="h4">
+              RECURSO ESPECIAL No 1.038.032 - RJ
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              STJ
+            </Typography>
+            <Typography variant="body2" className={classes.infoText} component="p">
+              A marca de alto renome (art. 125 daLei de Propriedade Industrial LPI) é exceção ao
+              principio da especifidade e tem proteção especial em todos os ramos de atividade,
+              desde que previamente registrada no Brasil e assim declarada pelo INPI - Instituto
+              Nacional de Propriedade Insdustrial
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              style={{
+                background: '#FF8C00',
+                color: '#fff',
+                marginLeft: '10px',
+                marginRight: '30%',
+              }}
+              disabled={true}
+              variant="contained"
+              size="small">
+              MARCA
+            </Button>
+            <Button
+              style={{ background: '#1351B4', color: '#fff' }}
+              variant="contained"
+              size="small">
+              Ver Mais
+            </Button>
+          </CardActions>
+        </Card>
+        <Card className={classes.root}>
+          <CardContent>
+            <Typography className={classes.title} variant="h6" component="h4">
+              RECURSO ESPECIAL No 1.038.032 - RJ
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              STJ
+            </Typography>
+            <Typography variant="body2" className={classes.infoText} component="p">
+              A marca de alto renome (art. 125 daLei de Propriedade Industrial LPI) é exceção ao
+              principio da especifidade e tem proteção especial em todos os ramos de atividade,
+              desde que previamente registrada no Brasil e assim declarada pelo INPI - Instituto
+              Nacional de Propriedade Insdustrial
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              style={{
+                background: '#FF8C00',
+                color: '#fff',
+                marginLeft: '10px',
+                marginRight: '30%',
+              }}
+              disabled={true}
+              variant="contained"
+              size="small">
+              MARCA
+            </Button>
+            <Button
+              style={{ background: '#1351B4', color: '#fff' }}
+              variant="contained"
+              size="small">
+              Ver Mais
+            </Button>
+          </CardActions>
+        </Card>
+      </Grid>
     </Container>
   );
 };
